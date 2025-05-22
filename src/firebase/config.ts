1e8f99b95b70d4053f4ca5a8f5aee0a7177aa3b0
YESHWANTHB03 <124280153+YESHWANTHB03@users.noreply.github.com>
2025-05-22 12:59:37 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,19 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 //THIS IS THE MAIN DB
-/*const firebaseConfig = {
-  apiKey: "AIzaSyA-pRxNph8ECQEi2IzUWgPAq06fi-3wKK8",
-  authDomain: "bossinn-7fbb9.firebaseapp.com",
-  projectId: "bossinn-7fbb9",
-  storageBucket: "bossinn-7fbb9.firebasestorage.app",
-  messagingSenderId: "138358328390",
-  appId: "1:138358328390:web:e04ab1e548d7c56e5fce47",
-  measurementId: "G-8KRR5T7YJQ"
-};
-*/
-
-//TESTING
-
 const firebaseConfig = {
   apiKey: "AIzaSyA-pRxNph8ECQEi2IzUWgPAq06fi-3wKK8",
   authDomain: "bossinn-7fbb9.firebaseapp.com",
@@ -25,6 +12,19 @@ const firebaseConfig = {
   appId: "1:138358328390:web:e04ab1e548d7c56e5fce47",
   measurementId: "G-8KRR5T7YJQ"
 };
+
+
+//TESTING
+
+/*const firebaseConfig = {
+  apiKey: "AIzaSyA-pRxNph8ECQEi2IzUWgPAq06fi-3wKK8",
+  authDomain: "bossinn-7fbb9.firebaseapp.com",
+  projectId: "bossinn-7fbb9",
+  storageBucket: "bossinn-7fbb9.firebasestorage.app",
+  messagingSenderId: "138358328390",
+  appId: "1:138358328390:web:e04ab1e548d7c56e5fce47",
+  measurementId: "G-8KRR5T7YJQ"
+};*/
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
