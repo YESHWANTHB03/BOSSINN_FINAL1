234e380d21c3bc7235a5e5f9a0ffbe5f11a5910c
YESHWANTHB03 <124280153+YESHWANTHB03@users.noreply.github.com>
2025-05-22 13:04:40 +0530

    Update config.ts

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,13 +4,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 //THIS IS THE MAIN DB
 const firebaseConfig = {
-  apiKey: "AIzaSyA-pRxNph8ECQEi2IzUWgPAq06fi-3wKK8",
-  authDomain: "bossinn-7fbb9.firebaseapp.com",
-  projectId: "bossinn-7fbb9",
-  storageBucket: "bossinn-7fbb9.firebasestorage.app",
-  messagingSenderId: "138358328390",
-  appId: "1:138358328390:web:e04ab1e548d7c56e5fce47",
-  measurementId: "G-8KRR5T7YJQ"
+  apiKey: "AIzaSyD57ySp5pnHPxX_3UaveoC62lZHFOUrT-Y",
+  authDomain: "bossinn-44651.firebaseapp.com",
+  projectId: "bossinn-44651",
+  storageBucket: "bossinn-44651.firebasestorage.app",
+  messagingSenderId: "510548886376",
+  appId: "1:510548886376:web:2a4d9f0e8724090cc7ccdd",
+  measurementId: "G-596DPL3V12"
 };
 
 
